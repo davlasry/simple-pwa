a3d7a2b6bfb8e7c9849b3daf8b2957900e55295c
davidlasry <lasry.david@gmail.com>
2020-12-28 17:55:20 +0200

    service worker

--- sw.js
+++ sw.js
@@ -1,32 +1,39 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-workbox.routing.registerRoute(
-    /\.(?:css|js)$/,
-    new workbox.strategies.StaleWhileRevalidate({
-        "cacheName": "assets",
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxEntries: 1000,
-                maxAgeSeconds: 31536000
-            })
-        ]
-    })
-);
-
-workbox.routing.registerRoute(
-    /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
-    new workbox.strategies.CacheFirst({
-        "cacheName": "images",
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxEntries: 1000,
-                maxAgeSeconds: 31536000
-            })
-        ]
-    })
-);
+// workbox.routing.registerRoute(
+//     /\.(?:css|js)$/,
+//     new workbox.strategies.StaleWhileRevalidate({
+//         "cacheName": "assets",
+//         plugins: [
+//             new workbox.expiration.Plugin({
+//                 maxEntries: 1000,
+//                 maxAgeSeconds: 31536000
+//             })
+//         ]
+//     })
+// );
+//
+// workbox.routing.registerRoute(
+//     /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+//     new workbox.strategies.CacheFirst({
+//         "cacheName": "images",
+//         plugins: [
+//             new workbox.expiration.Plugin({
+//                 maxEntries: 1000,
+//                 maxAgeSeconds: 31536000
+//             })
+//         ]
+//     })
+// );
 
 self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('simple-pwa')
+            .then(cache => {
+                console.log(cache);
+                return cache.addAll(urlsToCache);
+            })
+    )
     console.log('Service worker installed');
 })
 
@@ -34,8 +41,18 @@ self.addEventListener('activate', event => {
     console.log('Service worker activated');
 })
 
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    )
+})
 
-urlsToCache =[
+urlsToCache = [
     '/',
-    '/images/image.png'
+    '/images/image.png',
+    '/scripts/app.js',
+    '/scripts/static.js'
 ]
